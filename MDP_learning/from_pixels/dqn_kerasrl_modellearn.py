8877f198843bff6586f4b3cceb62777833946060
Florian Gawrilowicz <floicz@gmail.com>
2017-12-07 00:36:39 +0100

    - changing sequence target to state1 - changing activation and size of LSTM

--- MDP_learning/from_pixels/dqn_kerasrl_modellearn.py
+++ MDP_learning/from_pixels/dqn_kerasrl_modellearn.py
@@ -6,11 +6,10 @@ from PIL import Image
 import numpy as np
 import gym
 
-from keras.models import Sequential, Model
-from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute, Input, Embedding, LSTM, concatenate, \
-    Reshape
+from keras.models import Model
+from keras.layers import Dense, Flatten, Conv2D, Permute, Input, LSTM, concatenate, Reshape
 from keras.optimizers import Adam
-import keras.backend as K
+from keras.callbacks import TensorBoard
 
 from rl.agents.dqn import DQNAgent
 from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
@@ -18,12 +17,10 @@ from rl.memory import SequentialMemory
 from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
-from keras.callbacks import TensorBoard
-
 INPUT_SHAPE = (84, 84)
 WINDOW_LENGTH = 4
-nb_steps_dqn_fit = 1750000  # 1750000
-nb_steps_warmup_dqn_agent = int(max(0, np.sqrt(nb_steps_dqn_fit))) * 42 + 1000  # 50000
+nb_steps_dqn_fit = 123456  # 1750000
+nb_steps_warmup_dqn_agent = int(max(0, np.sqrt(nb_steps_dqn_fit))) * 42 + 42  # 50000
 target_model_update_dqn_agent = int(max(0, np.sqrt(nb_steps_dqn_fit))) * 8 + 8  # 10000
 memory_limit = nb_steps_dqn_fit  # 1000000
 nb_steps_annealed_policy = int(nb_steps_dqn_fit / 2)  # 1000000
@@ -66,9 +63,9 @@ input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 
 image_in = Input(shape=input_shape, name='main_input')
 input_perm = Permute((2, 3, 1), input_shape=input_shape)(image_in)
-conv1 = Convolution2D(32, 8, 8, subsample=(4, 4), activation='relu')(input_perm)
-conv2 = Convolution2D(64, 4, 4, subsample=(2, 2), activation='relu')(conv1)
-conv3 = Convolution2D(64, 3, 3, subsample=(1, 1), activation='relu')(conv2)
+conv1 = Conv2D(32, (8, 8), activation="relu", strides=(4, 4))(input_perm)
+conv2 = Conv2D(64, (4, 4), activation="relu", strides=(2, 2))(conv1)
+conv3 = Conv2D(64, (3, 3), activation="relu", strides=(1, 1))(conv2)
 conv_out = Flatten(name='flat_feat')(conv3)
 dense_out = Dense(512, activation='relu')(conv_out)
 q_out = Dense(nb_actions, activation='linear')(dense_out)
@@ -82,13 +79,13 @@ action_in = Input(shape=action_shape, name='action_input')
 enc_state = Input(shape=(sequence_length, int(np.prod(conv3.shape[1:]))), name='enc_state')
 action_in_reshape = Reshape((sequence_length, 1))(action_in)
 enc_state_and_action = concatenate([enc_state, action_in_reshape], name='encoded_state_and_action')
-lstm_out = LSTM(512)(enc_state_and_action)
-state_pred = Dense(int(np.prod(conv3.shape[1:])), activation='relu', name='predicted_next_state')(lstm_out)
+lstm_out = LSTM(4096)(enc_state_and_action)
+state_pred = Dense(int(np.prod(conv3.shape[1:])), activation='linear', name='predicted_next_state')(lstm_out)
 reward_pred = Dense(1, activation='linear', name='predicted_reward')(lstm_out)
 terminal_pred = Dense(1, activation='sigmoid', name='predicted_terminal')(lstm_out)
 ml_model = Model(inputs=[enc_state, action_in], outputs=[state_pred, reward_pred, terminal_pred])
-ml_model.compile('rmsprop', loss={'predicted_next_state': 'mae', 'predicted_reward': 'mse',
-                                  'predicted_terminal': 'binary_crossentropy'}, metrics=['accuracy'])
+ml_model.compile('adam', loss={'predicted_next_state': 'mae', 'predicted_reward': 'mse',
+                               'predicted_terminal': 'binary_crossentropy'}, metrics=['accuracy'])
 print(ml_model.summary())
 
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
@@ -136,9 +133,9 @@ if args.mode == 'train':
     print(model_truncated.summary())
 
     data_size = dqn.memory.observations.length
-    batch_size = 50000
-    n_epochs = 9 * int(data_size / batch_size) + 1  # go through data n times
-    for ii in range(n_epochs):
+    batch_size = int(data_size / 5)
+    n_rounds = 2 * int(data_size / batch_size) + 1  # go through data 2 times
+    for ii in range(n_rounds):
         hstates = np.empty((batch_size, sequence_length, int(np.prod(conv3.shape[1:]))), dtype=np.float32)
         actions = np.empty((batch_size, sequence_length, 1), dtype=np.float32)
         next_hstate = np.empty((batch_size, int(np.prod(conv3.shape[1:]))), dtype=np.float32)
@@ -147,42 +144,47 @@ if args.mode == 'train':
 
         for jj in range(batch_size):
             # check for terminals
-            start = random.randrange(data_size - (sequence_length + 1))
-            experiences = dqn.memory.sample(sequence_length + 1, range(start, start + sequence_length + 1))
+            start = random.randrange(data_size - sequence_length)
+            experiences = dqn.memory.sample(sequence_length, range(start, start + sequence_length))
             while np.array([e.terminal1 for e in experiences]).any():
-                start = random.randrange(data_size - (sequence_length + 1))
-                experiences = dqn.memory.sample(sequence_length + 1, range(start, start + sequence_length + 1))
+                if start is 0:
+                    start = random.randrange(data_size - sequence_length)
+                else:
+                    start -= 1 # get a bit more terminal
+                experiences = dqn.memory.sample(sequence_length, range(start, start + sequence_length))
 
             # Start by extracting the necessary parameters (we use a vectorized implementation).
             state0_seq = []
-            # state1_batch = []
+            state1_seq = []
             reward_seq = []
             action_seq = []
             terminal1_seq = []
 
             for e in experiences:
                 state0_seq.append(e.state0)
-                # state1_batch.append(e.state1)
+                state1_seq.append(e.state1)
                 reward_seq.append(e.reward)
                 action_seq.append(e.action)
                 terminal1_seq.append(e.terminal1)
 
+            # TODO consider differencing
             state0_seq = dqn.process_state_batch(state0_seq)
-            # state1_batch = dqn.process_state_batch(state1_batch)
+            state1_seq = dqn.process_state_batch(state1_seq)
             reward_seq = np.array(reward_seq)
             action_seq = np.array(action_seq, dtype=np.float32)
             terminal1_seq = np.array(terminal1_seq)
 
-            hidden_states_seq = model_truncated.predict_on_batch(state0_seq)
+            hidden_state0_seq = model_truncated.predict_on_batch(state0_seq)
+            hidden_state1_seq = model_truncated.predict_on_batch(state1_seq)
 
-            hstates[jj, ...] = hidden_states_seq[np.newaxis, :-1, :]
-            actions[jj, ...] = action_seq[np.newaxis, :-1, np.newaxis]
-            next_hstate[jj, ...] = hidden_states_seq[np.newaxis, -1, :]
+            hstates[jj, ...] = hidden_state0_seq[np.newaxis, :, :]
+            actions[jj, ...] = action_seq[np.newaxis, :, np.newaxis]
+            next_hstate[jj, ...] = hidden_state1_seq[np.newaxis, -1, :]
             rewards[jj, ...] = reward_seq[np.newaxis, -1]
             terminals[jj, ...] = terminal1_seq[np.newaxis, -1]
 
-        ml_model.fit([hstates, actions], [next_hstate, rewards, terminals], verbose=1, epochs=2,
-                     callbacks=[TensorBoard(log_dir='./logs/Tlearn')])
+        ml_model.fit([hstates, actions], [next_hstate, rewards, terminals], verbose=1, epochs=4,
+                     callbacks=[TensorBoard(log_dir='./logs/Tlearn')], shuffle=False)
 
     # #######################################################################################################################
     from collections import deque
@@ -230,6 +232,7 @@ if args.mode == 'train':
             self.state_seq.append(next_state)
             # unwrap and add empty info
             return next_state[0], float(reward[0, 0]), bool(done[0, 0] > .5), {}
+
         # TODO done might never occur in unseen territory
         # TODO check timing t->t+1
 
@@ -261,9 +264,9 @@ if args.mode == 'train':
 
     image_in = Input(shape=input_shape, name='main_input')
     input_perm = Permute((2, 3, 1), input_shape=input_shape)(image_in)
-    conv1 = Convolution2D(32, 8, 8, subsample=(4, 4), activation='relu')(input_perm)
-    conv2 = Convolution2D(64, 4, 4, subsample=(2, 2), activation='relu')(conv1)
-    conv3 = Convolution2D(64, 3, 3, subsample=(1, 1), activation='relu')(conv2)
+    conv1 = Conv2D(32, (8, 8), activation="relu", strides=(4, 4))(input_perm)
+    conv2 = Conv2D(64, (4, 4), activation="relu", strides=(2, 2))(conv1)
+    conv3 = Conv2D(64, (3, 3), activation="relu", strides=(1, 1))(conv2)
     conv_out = Flatten(name='flat_feat')(conv3)
     dense_out = Dense(512, activation='relu')(conv_out)
     q_out = Dense(nb_actions, activation='linear')(dense_out)
