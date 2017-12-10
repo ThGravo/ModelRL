05d66ebcbc8efc27c990b44af49596355a183171
Florian Gawrilowicz <floicz@gmail.com>
2017-12-10 15:18:05 +0100

    deep dense pong

--- MDP_learning/from_pixels/dqn_kerasrl_modellearn.py
+++ MDP_learning/from_pixels/dqn_kerasrl_modellearn.py
@@ -48,7 +48,7 @@ class AtariProcessor(Processor):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', choices=['train', 'test'], default='train')
-parser.add_argument('--env-name', type=str, default='BreakoutDeterministic-v4')
+parser.add_argument('--env-name', type=str, default='PongDeterministic-v4')
 parser.add_argument('--weights', type=str, default=None)
 args = parser.parse_args()
 
@@ -74,7 +74,7 @@ print(model.summary())
 hstate_size = int(np.prod(conv3.shape[1:]))
 
 # Model learner network
-USE_LSTM = True
+USE_LSTM = False
 sequence_length = 5 if USE_LSTM else 1
 action_shape = (sequence_length, 1)  # TODO: get shape from environment. something like env.action.space.shape?
 action_in = Input(shape=action_shape, name='action_input')
@@ -87,7 +87,8 @@ else:
     action_in_flat = Flatten(name='flat_act')(action_in)
     enc_state_flat = Flatten(name='flat_state')(enc_state)
     enc_state_and_action = concatenate([enc_state_flat, action_in_flat], name='encoded_state_and_action')
-    lstm_out = Dense(8192, activation='relu')(enc_state_and_action)
+    dense_out = Dense(8192, activation='relu')(enc_state_and_action)
+    lstm_out = Dense(4096, activation='relu')(dense_out)
 
 state_pred = Dense(hstate_size, activation='linear', name='predicted_next_state')(lstm_out)
 reward_pred = Dense(1, activation='linear', name='predicted_reward')(lstm_out)
