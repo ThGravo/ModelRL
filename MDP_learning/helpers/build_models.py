f9f805f5d00bf24f73ed1dcc725590999246f936
Florian Gawrilowicz <floicz@gmail.com>
2017-12-28 14:21:55 +0100

    renaming size parameter, since it is just the constant factor for the dim_multipliers

--- MDP_learning/helpers/build_models.py
+++ MDP_learning/helpers/build_models.py
@@ -7,47 +7,49 @@ from keras.models import Sequential
 # state and action is input and successor state is output
 def build_regression_model(input_dim,
                            output_dim,
-                           state_size,
-                           dim_multipliers=(16, 8),
-                           activations=('sigmoid', 'sigmoid'),
+                           base_size=None, # base_size for adaptive size
+                           dim_multipliers=(32, 16),
+                           activations=('relu', 'sigmoid'),
                            lr=.001,
                            recurrent=False):
+    base_size = input_dim if base_size is None else base_size
     num_hlayers = len(dim_multipliers)
     model = Sequential()
     if recurrent:
-        model.add(LSTM(state_size * dim_multipliers[0],
+        model.add(LSTM(base_size * dim_multipliers[0],
                        input_shape=(None, input_dim),
                        activation=activations[0],
                        return_sequences=num_hlayers is not 1))
         for i in range(num_hlayers - 1):
             # stacked LSTMs need to return a sequence
-            model.add(LSTM(state_size * dim_multipliers[i + 1],
+            model.add(LSTM(base_size * dim_multipliers[i + 1],
                            activation=activations[min(i + 1, len(activations) - 1)],
                            return_sequences=i is not num_hlayers - 2))
         model.add(Dense(output_dim, activation='linear'))
         model.compile(loss='mse', optimizer=Adam(lr=lr), metrics=['accuracy'])
     else:
-        model.add(Dense(state_size * dim_multipliers[0], input_dim=input_dim, activation=activations[0]))
+        model.add(Dense(base_size * dim_multipliers[0], input_dim=input_dim, activation=activations[0]))
         for i in range(num_hlayers - 1):
-            model.add(Dense(state_size * dim_multipliers[i + 1],
+            model.add(Dense(base_size * dim_multipliers[i + 1],
                             activation=activations[min(i + 1, len(activations) - 1)]))
         model.add(Dense(output_dim, activation='linear'))
         model.compile(loss='mse', optimizer=Adam(lr=lr), metrics=['accuracy'])
-    model.summary()
+    # model.summary()
     return model
 
 
 # approximate Done value
 # state is input and reward is output
 def build_dmodel(input_dim,
-                 state_size,
+                 base_size=None,
                  dim_multipliers=(14, 8),
                  activations=('sigmoid', 'sigmoid'),
                  lr=.001):
+    base_size = input_dim if base_size is None else base_size
     model = Sequential()
-    model.add(Dense(state_size * dim_multipliers[0], input_dim=input_dim, activation=activations[0]))
+    model.add(Dense(base_size * dim_multipliers[0], input_dim=input_dim, activation=activations[0]))
     for i in range(len(dim_multipliers) - 1):
-        model.add(Dense(state_size * dim_multipliers[i + 1], activation=activations[i + 1]))
+        model.add(Dense(base_size * dim_multipliers[i + 1], activation=activations[i + 1]))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer=Adam(lr=lr), metrics=['accuracy'])
     # model.summary()
