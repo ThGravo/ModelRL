4ce72b6ce18cb8de28a6a1105e172aed1af0f7c3
Florian Gawrilowicz <floicz@gmail.com>
2018-01-02 15:40:22 +0100

    fixed the dimenionality issue

--- MDP_learning/helpers/custom_metrics.py
+++ MDP_learning/helpers/custom_metrics.py
@@ -2,14 +2,18 @@ from keras import backend as K
 
 
 def COD(y_true, y_pred):
-    return K.sum(K.square(y_pred - y_true), axis=-1) / K.sum(K.square(y_pred - K.mean(y_pred, axis=-1)), axis=-1)
+    sse = K.sum(K.square(y_pred - y_true), axis=0)
+    scale = K.sum(K.square(y_pred - K.mean(y_pred, axis=0)), axis=0)
+    return K.mean(sse / (scale + K.epsilon()), axis=-1)
 
 
 def Rsquared(y_true, y_pred):
-    SS_res = K.sum(K.square(y_true - y_pred), axis=-1)
-    SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
-    return 1.0 - SS_res / (SS_tot + K.epsilon())
+    sse = K.sum(K.square(y_true - y_pred), axis=0)
+    tot = K.sum(K.square(y_true - K.mean(y_true, axis=0)), axis=0)
+    return K.mean(1.0 - sse / (tot + K.epsilon()), axis=-1)
 
 
 def NRMSE(y_true, y_pred):
-    return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1)) / K.mean((K.max(y_true, axis=-1) - K.min(y_true, axis=-1)))
+    mse = K.mean(K.square(y_pred - y_true), axis=0)
+    scale = K.max(y_true, axis=0) - K.min(y_true, axis=0)
+    return K.mean(K.sqrt(mse) / (scale + K.epsilon()), axis=-1)
