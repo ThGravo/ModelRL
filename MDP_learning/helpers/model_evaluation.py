e83fdca511720619962bff890f5a7783c1add393
Florian Gawrilowicz <floicz@gmail.com>
2018-01-02 16:17:26 +0100

    sklearn R^2 eval wrapper

---
+++ MDP_learning/helpers/model_evaluation.py
@@ -0,0 +1,14 @@
+def sk_eval(model, input_data, train_signal, outfile_name=None):
+    from sklearn.metrics import r2_score
+    text_file = open("OutputAtariDataStats.txt", "w") if outfile_name is not None else None
+
+    y_pred = model.predict(input_data)
+    print('r2_score(uniform_average): '
+          '{}'.format(r2_score(train_signal, y_pred, multioutput='uniform_average')),
+          file=text_file)
+    print('r2_score(variance_weighted): '
+          '{}'.format(r2_score(train_signal, y_pred, multioutput='variance_weighted')),
+          file=text_file)
+    print('r2_score(raw_values): '
+          '{}'.format(r2_score(train_signal, y_pred, multioutput='raw_values')),
+          file=text_file)
