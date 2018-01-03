6e2934e1dc2a437bf6a282d2c54ee4448563baea
Florian Gawrilowicz <floicz@gmail.com>
2018-01-03 23:40:47 +0100

    committing

--- MDP_learning/single_agent/preprocessing.py
+++ MDP_learning/single_agent/preprocessing.py
@@ -14,20 +14,6 @@ def standardise_memory(memory, state_size, action_size):
     memory[:, - state_size - 1:-1] = states_scaled[len(memory):, :state_size]
     memory[:, state_size: state_size + action_size] = actions_scaled
 
-def partmem(memory, state_size, partial_obs_rate):
-    masks_states = np.random.choice([-1.0, 1.0], size=(len(memory), state_size),
-                                    p=[partial_obs_rate, 1 - partial_obs_rate])
-    masks_next_states = np.random.choice([-1.0, 1.0], size=(len(memory), state_size),
-                                         p=[partial_obs_rate, 1 - partial_obs_rate])
-    for i in range(len(memory)):
-        if i == 0 or memory[i - 1, -1]:
-            memory[i, :state_size] = masks_states[i] * memory[i, :state_size]
-            memory[i, - state_size - 1:-1] = masks_next_states[i] * memory[i, - state_size - 1:-1]
-        else:
-            memory[i, :state_size] = memory[i - 1, -state_size - 1:-1]
-            memory[i, - state_size - 1:-1] = masks_next_states[i] * memory[i, - state_size - 1:-1]
-
-    memory[memory < 0] = -1
 
 def make_mem_partial_obs(memory, state_size, partial_obs_rate):
     masks_states = np.random.choice([np.nan, 1.0], size=(len(memory), state_size),
@@ -44,7 +30,7 @@ def make_mem_partial_obs(memory, state_size, partial_obs_rate):
 
 def impute_missing(memory, state_size, imputer):
     states = np.vstack((memory[:, :state_size], memory[:, - state_size - 1:-1]))
-    states_imputed = imputer().complete(states)
+    states_imputed = imputer(n_imputations=50).complete(states)
     memory[:, :state_size] = states_imputed[:len(memory), :state_size]
     memory[:, - state_size - 1:-1] = states_imputed[len(memory):, :state_size]
 
