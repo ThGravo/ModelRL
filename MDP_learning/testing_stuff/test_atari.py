336b4bdd1104845e4c050fb1d3fe9460facddb2e
Florian Gawrilowicz <floicz@gmail.com>
2018-08-12 16:32:50 +0200

    adapting to new interface

--- MDP_learning/testing_stuff/test_atari.py
+++ MDP_learning/testing_stuff/test_atari.py
@@ -2,12 +2,13 @@ from MDP_learning.from_pixels import dqn_kerasrl_modellearn
 import gym
 
 
-env_name = 'SeaquestDeterministic-v4'
+env_name = 'Seaquest-v4'
 environment = gym.make(env_name)
 num_actions = environment.action_space.n
 
 INPUT_SHAPE = (84, 84)
 processor = dqn_kerasrl_modellearn.AtariProcessor(INPUT_SHAPE)
-dqn_agent, hidden_state_size = dqn_kerasrl_modellearn.setupDQN(num_actions, processor)
+atariCfg = dqn_kerasrl_modellearn.AtariConfig(env_name)
+dqn_agent = dqn_kerasrl_modellearn.setupDQN(atariCfg, num_actions, processor)
 
-dqn_kerasrl_modellearn.trainDQN(environment, dqn_agent)
+dqn_kerasrl_modellearn.trainDQN(atariCfg, environment, dqn_agent)
