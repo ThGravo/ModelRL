8116e6ffd6f72e9c96b0df2607ad7c81c2648330
Florian Gawrilowicz <floicz@gmail.com>
2018-08-30 16:16:28 +0200

    adapable loss weights

--- MDP_learning/from_pixels/trainICM.py
+++ MDP_learning/from_pixels/trainICM.py
@@ -1,12 +1,33 @@
 import numpy as np
 from keras.models import Model
 from keras.layers import Dense, Flatten, Conv2D, Permute, Input, LSTM, concatenate, Reshape
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, Callback
 from keras.utils import to_categorical
 from keras.losses import mse, mape
+import keras.backend as K
 import time
 import os
 from MDP_learning.helpers.custom_metrics import COD, NRMSE, Rsquared
+
+
+# and define your own Callback
+
+class MyCallback(Callback):
+    def __init__(self, alpha, beta):
+        self.alpha = alpha
+        self.beta = beta
+
+    # customize your behavior
+    def on_epoch_end(self, epoch, logs={}):
+        K.set_value(self.alpha, max(K.get_value(self.alpha) / 1.1, .01))
+        # K.set_value(self.beta, K.get_value(self.beta) * 1.01)
+        # logger.info("epoch %s, alpha = %s, beta = %s" % (epoch, K.get_value(self.alpha), K.get_value(self.beta)))
+        print("epoch %s, alpha = %s, beta = %s" % (epoch, K.get_value(self.alpha), K.get_value(self.beta)))
+
+
+# then pass it into fit
+
+# model.fit( ..., callbacks=[MyCallback(alpha, beta)], ...)
 
 
 def train_icm(cfg, dqn, nb_actions, do_load_dqn_weights=True):
@@ -44,14 +65,16 @@ def train_icm(cfg, dqn, nb_actions, do_load_dqn_weights=True):
     state_pred = Dense(encoded_0._keras_shape[1], activation='linear', name='fmodel')(fc)
 
     def custom_loss(y_true, y_pred):
-        return mape(encoded_1, y_pred)
+        return mse(encoded_1, y_pred)
 
     def custom_metric(y_true, y_pred):
         return mse(encoded_1[:, :y_pred.shape[1]], y_true)
 
+    alpha = K.variable(5000)
+    beta = K.variable(1)
     im_model = Model(inputs=[image0_in, image1_in, action_in], outputs=[a_out, state_pred])
     im_model.compile(optimizer='nadam', loss=['sparse_categorical_crossentropy', custom_loss],
-                     metrics=['sparse_categorical_accuracy', mape, custom_metric], loss_weights=[.1, 100])
+                     metrics=['sparse_categorical_accuracy', mape, custom_metric], loss_weights=[alpha, beta])
 
     if do_load_dqn_weights:
         shared_conv_model.load_weights(cfg.filename, by_name=True, skip_mismatch=True)
@@ -115,23 +138,16 @@ def train_icm(cfg, dqn, nb_actions, do_load_dqn_weights=True):
 
     print(im_model.summary())
 
-    log_string = 'ICM_{}_nb_actions{}_loadDQNwgths{}-{}'.format(cfg.env_name, nb_actions, do_load_dqn_weights,
+    log_string = 'dyna_{}_nb_actions{}_loadDQNwgths{}-{}'.format(cfg.env_name, nb_actions, do_load_dqn_weights,
                                                                 time.time())
     print('logging to {}'.format(log_string))
     ####################################################################################################################
 
     data_size = dqn.memory.observations.length
-    chunk_size = max(min(data_size - 5, 100000), int(data_size / 100))
-    n_rounds = 2 * int(data_size / chunk_size) + 1  # go through data 2 times
+    chunk_size = int(max(min(data_size / 2, 10000), data_size / 100))
+    n_rounds = 2 * int(data_size / chunk_size) + 1  # go through data 3 times
     for ii in range(n_rounds):
         print("{} of {} n_rounds".format(ii, n_rounds))
-        states = np.empty((chunk_size,) + state_size, dtype=np.float32)
-        next_state = np.empty((chunk_size,) + state_size, dtype=np.float32)
-        actions = np.empty((chunk_size, nb_actions), dtype=np.float32)
-        rewards = np.empty((chunk_size, 1), dtype=np.float32)
-        terminals = np.empty((chunk_size, 1), dtype=np.float32)
-        empty_hstates = np.empty((chunk_size, encoded_0._keras_shape[1]), dtype=np.float32)
-
         experiences = dqn.memory.sample(chunk_size)
         # Start by extracting the necessary parameters
         state0_seq = []
@@ -166,7 +182,12 @@ def train_icm(cfg, dqn, nb_actions, do_load_dqn_weights=True):
                 print("Max: {}".format(np.max(next_state, axis=0)), file=text_file)
 
         im_model.fit([state0_seq, state1_seq, actions_encoded], [action_seq, hstate1_seq],
-                     validation_split=0.2, verbose=1, epochs=cfg.ml_model_epochs, callbacks=[
-                TensorBoard(log_dir='./invmod_logs/{}/{}'.format(os.path.splitext(cfg.filename)[0], log_string))])
+                     validation_split=0.2, verbose=1, epochs=cfg.ml_model_epochs, callbacks=[MyCallback(alpha, beta),
+                                                                                             TensorBoard(
+                                                                                                 log_dir='./invmod_logs/{}/{}'.format(
+                                                                                                     os.path.splitext(
+                                                                                                         cfg.filename)[
+                                                                                                         0],
+                                                                                                     log_string))])
     return im_model
     # #######################################################################################################################
